662600c4add8a56497d88afc2743e880bc5a79cc
wangshungen <1941325247@qq.com>
2021-11-13 21:33:31 +0800

    第一次更新

---
+++ TestGit/src/main/java/com/shungen/git/Dog.java
@@ -0,0 +1,14 @@
+package com.shungen.git;
+
+/**
+ * @author wangshungen
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/11/13 21:29
+ */
+public class Dog {
+    public static void show(){
+        System.out.println("我是一条狗");
+        System.out.println("更新1");
+    }
+}
